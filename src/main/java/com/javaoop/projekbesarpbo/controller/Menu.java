f2f5ad8d216fa4884985ea3ad7fa74c465f56291
paldooy <kemoywashere@gmail.com>
2024-05-22 09:10:51 +0800

    gui nya

---
+++ src/main/java/com/javaoop/projekbesarpbo/controller/Menu.java
@@ -0,0 +1,107 @@
+package com.javaoop.projekbesarpbo.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Menu {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private MenuItem barangKeluarMenuItem;
+
+    @FXML
+    private MenuItem barangMasukMenuItem;
+
+    @FXML
+    private MenuItem cariBarangMenuItem;
+
+    @FXML
+    private MenuItem editBarangMenuItem;
+
+    @FXML
+    private MenuItem hapusBarangMenuItem;
+
+    @FXML
+    private MenuBar menu;
+
+    @FXML
+    private MenuItem menuUtamaxMenuItem;
+
+    @FXML
+    private MenuItem tambahBarangMenuItem;
+
+    @FXML
+    void menujuBarangMasuk(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("LogMasuk.fxml"));
+        stage = (Stage)((MenuItem)event.getSource()).getParentPopup().getOwnerWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void menujuBarangkeluar(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("LogKeluar.fxml"));
+        stage = (Stage)((MenuItem)event.getSource()).getParentPopup().getOwnerWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void menujuCari(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("../../../../../resources/com/javaoop/projekbesarpbo/Cari.fxml"));
+        stage = (Stage)((MenuItem)event.getSource()).getParentPopup().getOwnerWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void menujuEdit(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Edit.fxml"));
+        stage = (Stage)((MenuItem)event.getSource()).getParentPopup().getOwnerWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void menujuHapus(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Hapus.fxml"));
+        stage = (Stage)((MenuItem)event.getSource()).getParentPopup().getOwnerWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void menujuMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        stage = (Stage)((MenuItem)event.getSource()).getParentPopup().getOwnerWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void menujuTambah(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Tambah.fxml"));
+        stage = (Stage)((MenuItem)event.getSource()).getParentPopup().getOwnerWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
